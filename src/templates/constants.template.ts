7b8e5daa0cdf05ecec12785f0a044397422b0531
Corey <corey@dolomite.io>
2022-03-03 17:36:02 -0500

    deployed arbitrum rinkeby and arbitrum mainnet

--- src/templates/constants.template.ts
+++ src/templates/constants.template.ts
@@ -12,6 +12,8 @@ import {
 
 const MAINNET_NETWORK = 'mainnet'
 const MUMBAI_NETWORK = 'mumbai'
+const ARBITRUM_MAINNET_NETWORK = 'arbitrum-one'
+const ARBITRUM_RINKEBY_NETWORK = 'arbitrum-rinkeby'
 
 export const ZERO_BYTES = new Bytes(0)
 
@@ -47,7 +49,11 @@ export const EXPIRY_ADDRESS = '{{expiryAddress}}'.toLowerCase()
 
 export const DAI_ADDRESS = '{{daiAddress}}'.toLowerCase()
 
+export const LINK_ADDRESS = '{{linkAddress}}'.toLowerCase()
+
 export const USDC_ADDRESS = '{{usdcAddress}}'.toLowerCase()
+
+export const WBTC_ADDRESS = '{{wbtcAddress}}'.toLowerCase()
 
 export const WETH_ADDRESS = '{{wethAddress}}'.toLowerCase()
 
@@ -74,10 +80,10 @@ WHITELISTS.set(MAINNET_NETWORK, [
   '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
   '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
   '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
+  '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
+  '0x960b236a07cf122663c4303350609a66a7b288c0', // ANT
+  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
+  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
   '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8' // yCurv
 ])
 WHITELISTS.set(MUMBAI_NETWORK, [
@@ -86,4 +92,22 @@ WHITELISTS.set(MUMBAI_NETWORK, [
   DAI_ADDRESS,
   MATIC_ADDRESS
 ])
+WHITELISTS.set(ARBITRUM_MAINNET_NETWORK, [
+  WETH_ADDRESS,
+  USDC_ADDRESS,
+  DAI_ADDRESS,
+  WBTC_ADDRESS,
+  LINK_ADDRESS,
+])
+WHITELISTS.set(ARBITRUM_RINKEBY_NETWORK, [
+  WETH_ADDRESS,
+  USDC_ADDRESS,
+  DAI_ADDRESS,
+  WBTC_ADDRESS,
+  LINK_ADDRESS,
+])
 export const WHITELIST = WHITELISTS.get(NETWORK) as string[]
+
+if (WHITELIST.filter(value => value.toLowerCase() === ADDRESS_ZERO || value.length === 0).length > 0) {
+  throw new Error('Invalid item found in whitelist!')
+}
