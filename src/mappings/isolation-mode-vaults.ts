d4e03dd1634f6b8354bb98b056b8a8a6bc68eb64
Corey <corey@dolomite.io>
2024-04-08 15:59:38 -0400

    update schema for easier querying for iso vaults

--- src/mappings/isolation-mode-vaults.ts
+++ src/mappings/isolation-mode-vaults.ts
@@ -4,15 +4,16 @@ import { IsolationModeVaultReverseLookup, Token, User } from '../types/schema'
 import { createUserIfNecessary } from './helpers/user-helpers'
 
 export function handleVaultCreated(event: VaultCreatedEvent): void {
+  let token = Token.load(event.address.toHexString()) as Token
   createUserIfNecessary(event.params.vault)
   createUserIfNecessary(event.params.account)
 
   let vaultUser = User.load(event.params.vault.toHexString()) as User
   vaultUser.effectiveUser = event.params.account.toHexString()
   vaultUser.isEffectiveUser = false
+  vaultUser.isolationModeVault = token.id
   vaultUser.save()
 
-  let token = Token.load(event.address.toHexString()) as Token
   let vaultMap = new IsolationModeVaultReverseLookup(event.params.vault.toHexString())
   vaultMap.token = token.id
   vaultMap.vault = event.params.vault.toHexString()
